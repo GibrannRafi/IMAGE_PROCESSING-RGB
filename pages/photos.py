2679d6296ff64925beffa1c76b41cead2c7a63d5
Gibran Rafi <81399997+GibrannRafi@users.noreply.github.com>
2025-10-12 18:42:25 +0700

    Update photos.py

--- pages/photos.py
+++ pages/photos.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image, ImageOps, ImageEnhance
 import numpy as np
-import cv2
+
 
 st.set_page_config(page_title="🧠 Pengolahan Citra Digital", layout="wide")
 
